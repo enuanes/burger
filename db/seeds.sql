7f80228e45cfb4f78abe8cd12c4d86503a1289a2
Eric Nuanes <ecnuanes@outlook.com>
2018-04-29 14:45:20 -0700

    I created the db folder and coded the schema and seeds sql files.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Southwestern Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Western Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Black and Bleu Burger');
