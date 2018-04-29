e311b7bde33571ba6191680ae79bef46e260f184
Eric Nuanes <ecnuanes@outlook.com>
2018-04-29 15:49:36 -0700

    I finished adding menu items into the table.

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,19 @@ INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
 INSERT INTO burgers (burger_name) VALUES ('Southwestern Veggie Burger');
 INSERT INTO burgers (burger_name) VALUES ('Double Western Bacon Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Black and Bleu Burger');
+
+INSERT INTO menu (item) VALUES ('Mushroom Swiss Burger');
+INSERT INTO menu (item) VALUES ('Southwestern Veggie Burger');
+INSERT INTO menu (item) VALUES ('Double Western Bacon Cheeseburger');
+INSERT INTO menu (item) VALUES ('Black and Bleu Burger');
+INSERT INTO menu (item) VALUES ('Grilled Teriyaki Chicken Burger');
+INSERT INTO menu (item) VALUES ('Portobello Bacon Burger');
+INSERT INTO menu (item) VALUES ('Cheeseburger');
+INSERT INTO menu (item) VALUES ('Cowboy Whiskey Burger');
+
+INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Chili Cheese Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Garlic Fries', 0);
+INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
+INSERT INTO menu (item, entree) VALUES ('Garden Salad', 0);
+INSERT INTO menu (item, entree) VALUES ('Caesar Salad', 0);
