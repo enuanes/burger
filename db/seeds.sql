f9ded4e58d20e05abf5a78469d28a0204b41b602
Eric Nuanes <ecnuanes@outlook.com>
2018-04-29 17:18:48 -0700

    Finished entering seed data.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,7 @@
-INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
-INSERT INTO burgers (burger_name) VALUES ('Southwestern Veggie Burger');
-INSERT INTO burgers (burger_name) VALUES ('Double Western Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Black and Bleu Burger');
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pepper Jack Mushroom Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tequila Lime Burger', false);
 
-INSERT INTO menu (item) VALUES ('Mushroom Swiss Burger');
-INSERT INTO menu (item) VALUES ('Southwestern Veggie Burger');
-INSERT INTO menu (item) VALUES ('Double Western Bacon Cheeseburger');
-INSERT INTO menu (item) VALUES ('Black and Bleu Burger');
-INSERT INTO menu (item) VALUES ('Grilled Teriyaki Chicken Burger');
-INSERT INTO menu (item) VALUES ('Portobello Bacon Burger');
-INSERT INTO menu (item) VALUES ('Cheeseburger');
-INSERT INTO menu (item) VALUES ('Cowboy Whiskey Burger');
-
-INSERT INTO menu (item, entree) VALUES ('French Fries', 0);
-INSERT INTO menu (item, entree) VALUES ('Chili Cheese Fries', 0);
-INSERT INTO menu (item, entree) VALUES ('Garlic Fries', 0);
-INSERT INTO menu (item, entree) VALUES ('Onion Rings', 0);
-INSERT INTO menu (item, entree) VALUES ('Garden Salad', 0);
-INSERT INTO menu (item, entree) VALUES ('Caesar Salad', 0);
