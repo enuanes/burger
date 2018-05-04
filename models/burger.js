452569947f09af1e7762e435efb8e68aa922f390
Eric Nuanes <ecnuanes@outlook.com>
2018-05-03 20:20:10 -0700

    Finished coding burger.js file.

--- models/burger.js
+++ models/burger.js
@@ -3,27 +3,27 @@ var orm = require('../config/orm.js');
 
 // Create the burger object
 var burger = {
-    // Select all burger table entries
-    selectAll: function(cb) {
-        orm.selectAll('burgers', function(res) {
-            cb(res);
-        });
-    },
+  // Select all burger table entries
+  selectAll: function(cb) {
+    orm.selectAll('burgers', function(res) {
+      cb(res);
+    });
+  },
 
-    // The variables cols and vals are arrays
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne('burgers', cols, vals, function(res) {
-            cb(res);
-        });
-    },
+  // The variables cols and vals are arrays
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(res) {
+      cb(res);
+    });
+  },
 
-    // The objColsVals is an object specifying columns as object keys with associated values
-    updatOne: function(objColsVals, condition, cb) {
-        orm.updatOne('burgers', objColsVals, condition, function(res) {
-            cb(res);
-        });
-    }
+  // The objColVals is an object specifying columns as object keys with associated values
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
 };
 
-// Export the databas functions for the controller (burgers_controller.js)
+// Export the database functions for the controller (burgerController.js).
 module.exports = burger;
