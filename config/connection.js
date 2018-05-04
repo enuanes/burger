937b8e1d31f91863aea466db67130e245d065799
Eric Nuanes <ecnuanes@outlook.com>
2018-05-03 20:18:19 -0700

    Fixed mistakes in connection.js file.

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,30 @@
 // Pull in required dependencies
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 // Create the MySQL connection object
 var connection;
 
-if(process.env.JAWSDB_URL) {
-    // DB is JawsDB on Heroku
-    connection = mysql,createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    // DB is local on localhost
-    connection = mysql.createConnection({
-        port: 3000,
-        host: 'localhost',
-        user: 'root',
-        password: 'prat4lYf!',
-        database: 'burgers_db'
-    })
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'prat4lYf!',
+		database: 'burgers_db'
+	})
 };
 
 // Make the connection to MySQL
 connection.connect(function(err) {
-    if(err) {
-        console.error('error connecting: ' +err.stack);
-        return;
-    }
-
-    console.log('connected as id ' + connection.threadId);
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId);
 });
 
 // Export connection for ORM use
