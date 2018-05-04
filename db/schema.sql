7b2509ab13534d8ee3204636bd59fe725c129169
Eric Nuanes <ecnuanes@outlook.com>
2018-05-03 20:16:23 -0700

    Updated the schema.sql with the correct columns.

--- db/schema.sql
+++ db/schema.sql
@@ -5,11 +5,10 @@ USE burgers_db;
 
 -- Create a burgers table w/ the required fields
 CREATE TABLE burgers(
-    id INT AUTO_INCREMENT NOT NULL,
-    burger_name VARCHAR(50) NOT NULL,
-    devoured BOOLEAN DEFAULT false,
-    ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(id)
+    id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP,
+	PRIMARY KEY (id)
 );
 
